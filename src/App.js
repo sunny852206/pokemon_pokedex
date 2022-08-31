9fdafc6e68d97794e67fbf1397727cde77b8e547
Sunny Lau <Sunny@Yvonnes-MacBook-Air.local>
2022-08-31 14:35:50 -0700

    add basic structure of the front page and updated styles with css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1> Pokedex </h1>
+    <div className = "app-container">
+      <h1> Pokédex </h1>
+      <div className = "pokemon-container">
+        <div className = "all-container">
+
+        </div>
+        <button className = "load-more"> Load more</button>
+      </div>
     </div>
   );
 }
