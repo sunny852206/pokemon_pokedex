f64a793d4741ca2ed1d70e603f1108992195f5c3
Sunny Lau <sunnylau@Sunnys-Mac-mini.local>
2022-09-02 22:26:40 -0700

    able to fetch data from poke api on pokemon search function

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import "../styles/search.css";
+import Axios from "axios";
 
 function Search() {
+  const [pokemonName, setPokemonName] = useState("");
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
+      (response) => {
+        console.log(response);
+      }
+    );
+  };
+
   return (
     <div className="App">
       <div className="TitleSection">
         <h1>Pokemon Stats</h1>
-        <input type="text" />
-        <button> Search Pokemon</button>
+        <input
+          type="text"
+          onChange={(event) => {
+            //set pokemon name to event target value(user input)
+            setPokemonName(event.target.value);
+          }}
+        />
+        <button onClick={searchPokemon}> Search Pokemon</button>
       </div>
     </div>
   );
