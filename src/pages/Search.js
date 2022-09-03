1ffce36a9414eb65d1913992b20d27eda9b963e5
Sunny Lau <sunnylau@Sunnys-Mac-mini.local>
2022-09-02 21:56:08 -0700

    pokemon search component barebone with basic css styling

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,15 @@
+import "../styles/search.css";
+
 function Search() {
-  return <div>Search</div>;
+  return (
+    <div className="App">
+      <div className="TitleSection">
+        <h1>Pokemon Stats</h1>
+        <input type="text" />
+        <button> Search Pokemon</button>
+      </div>
+    </div>
+  );
 }
 
 export default Search;
